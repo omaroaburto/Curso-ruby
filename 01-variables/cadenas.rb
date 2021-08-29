3f32057ba29c6a7d04514ab8832e08db7f23ef6c
Omaro Alexis Aburto Leiva <oaburto@alumnos.ubiobio.cl>
2021-08-29 00:13:11 -0400

    variables

---
+++ 01-variables/cadenas.rb
@@ -0,0 +1,15 @@
+#cadena
+nombre = "omaro"
+#snake case
+apellido_persona = "aburto"
+#concatenación
+puts nombre+" "+apellido_persona
+
+#interpolación
+
+puts "\nEl nombre es #{nombre} y su apellido #{apellido_persona}"
+ 
+#Contar la cantidad de caracteres de una cadena
+puts nombre.length
+#convierte cadena en array
+puts nombre.chars
---
+++ 01-variables/numericas.rb
@@ -0,0 +1,11 @@
+#valor entenero
+numero = 3+5 
+puts numero
+numero = 3-5 
+puts numero 
+numero = 3/5 
+puts numero 
+numero = 3*5 
+puts numero 
+numero = 5%3 
+puts numero 
